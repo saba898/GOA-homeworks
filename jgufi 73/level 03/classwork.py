9a327f419180a94c5bf81a93222563ab7301114d
saba898 <sabazhorzholiani3@gmail.com>
2025-07-26 12:26:03 +0400

    Add files via upload

---
+++ jgufi 73/level 03/classwork.py
@@ -0,0 +1,25 @@
+number=(7)
+number2= (9.7)
+ 
+print ( 7 + 9.7)
+print(number + number2)
+
+
+N1= "7"
+N2= "9"
+
+print(N1 + N2)
+
+print ( 9+4)
+print("9"+ "9")
+print(9876543-3456789)
+print(7-7)
+
+
+
+print(5*5)
+print(5*"8")
+
+
+print(9/3)
+print(999/33)
---
+++ jgufi 73/level 03/homework.py
@@ -0,0 +1,44 @@
+num1= 7
+num2= 9
+num3= 8
+print(num1+num2+num3)
+
+x=578
+y=7853
+
+print(x+y)
+
+length=10
+width=20
+
+print(length*2+width*2)
+
+
+
+print(9876+987)
+print(3457483+738290)
+print(7658+45678)
+print(7655789+6547)
+print(12345678+987654321)
+
+
+
+print(9987654321-1234567899)
+print(6853819-1)
+print(84898-78549)
+print(5365745168262964863-57328953155489175)
+print(8-8)
+
+
+print(8*9)
+print(98689*0)
+print(6367*545)
+print(4637*565555)
+print(7*"7")
+
+
+print(9/3)
+print(333333/33)
+print(12345/5)
+print(345/3)
+print(99999/33333)
