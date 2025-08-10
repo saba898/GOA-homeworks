d9cac0dcd540ef034da79cc54dd50a1f125f1b68
saba898 <sabazhorzholiani3@gmail.com>
2025-08-10 12:11:57 +0400

    Add files via upload

---
+++ jgufi 73/level 05/claswork.py
@@ -0,0 +1,30 @@
+# 1name = "Saba"       # იწყება ციფრით
+# user-name = "Gio"    # შეიცავს "-"
+# user name = "Luka"   # შეიცავს space-ს
+# @age = 25            # შეიცავს "@"
+# for = "data"         # რეზერვირებული სიტყვა
+
+
+first_name = "Saba"
+last_name = "zhorzholiani"
+user_email = "sabazhorzholiani@mail.com"
+home_address = "Tbilisi, Georgia"
+favorite_color = "Blue"
+
+print(first_name)
+print(last_name)
+print(user_email)
+print(home_address)
+print(favorite_color)
+
+
+
+a= 7
+b=989
+print(a+b)
+
+print(a-b)
+
+print(a*b)
+
+print(b/a)
---
+++ jgufi 73/level 05/homework.py
@@ -0,0 +1,36 @@
+lname = "Luka"
+age_num = 56
+my_favmov = "Blood and Bone"
+my_friend_s_name = "Nika"
+
+
+print("My name is " + lname)
+print("My age is", age_num)
+print("My Favorite Movie is " + my_favmov)
+print("My Friend's name is " + my_friend_s_name)
+
+
+
+
+first_name = "Luka"
+last_name = "Kapanadze"
+my_favorite_color = "Blue"
+number_of_students = 25
+average_score = 89.5
+birth_year = 2005
+is_student = True
+total_price = 150.75
+current_temperature = 27
+best_friend_name="nugzari"
+
+
+num1 = 15
+num2 = 20
+
+num3= num1+num2   
+
+print(num1,num2,num3)
+
+
+
+
